bd0b75417079d9e84707ae2cf40557496f5948b5
Aaron Jiane <devmfundo@gmail.com>
2025-06-21 20:18:21 +0200

    Part2-Phonebook 2.9 step 4-before clean up

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,18 +1,30 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: "040-1234567"}
-  ]) 
+   const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [searcher, setSearcher] = useState('')
+
   const handleChange=(e)=>{
   	e.preventDefault()
   	const {id,value} = e.target
   	if(id==="name")setNewName(value.trim())
   	if(id==="phone-number")setNewNumber(value.trim())
+  	if(id==="search")setSearcher(value.trim())
   }
-
+  const filterContacts=(persons,searcher)=>{
+  	const filterOut=persons.filter((person)=>person.name.toLowerCase().includes(searcher.toLowerCase()))
+  	console.log(filterOut)
+  	return filterOut
+  }
+   const filtered = filterContacts(persons,searcher)
   const handleSave=(e)=>{
   	e.preventDefault()
   	if(!newName || !newNumber){
@@ -37,9 +49,13 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+      		filter shown with <input id="search" type="text" placeholder="search" onChange={handleChange} value={searcher}/>
+      </div>
+      <h2>add a new</h2>
       <form  onSubmit={handleSave}>
         <div>
-          name: <input id="name" type="text" placeholder="name" onChange={handleChange} value={newName}/>
+          name: <input id="name" type="text" placeholder="name" onChange={handleChange} value={newName}/><br/>
           number: <input id="phone-number" type="tel" placeholder="phone number" onChange={handleChange} value={newNumber}/>
         </div>
         <div>
@@ -47,7 +63,9 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person)=>(<p key={person.name}>{person.name} {person.number}</p>))}
+      {searcher.length > 0? 
+      filtered.map((person)=>(<p key={person.id}>{person.name} {person.number}</p>)) 
+      : persons.map((person)=>(<p key={person.id}>{person.name} {person.number}</p>))}
     </div>
   )
 }
