161ba17a3746c68a9f67e80732cf97c7c0b3e978
Aaron Jiane <devmfundo@gmail.com>
2025-06-21 12:26:12 +0200

    Part2-Phonebook 2.6 step 2

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+  const handleChange=(e)=>{
+  	e.preventDefault()
+  	const {value} = e.target
+  	setNewName(value)
+  }
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input id="name" placeholder="name" onChange={handleChange} value={newName}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>debug: {newName}</div>
+    </div>
+  )
+}
+
+export default App
