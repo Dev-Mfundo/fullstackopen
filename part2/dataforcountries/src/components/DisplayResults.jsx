b21ecf0ac13aaf90236a6e54aff45e88ad8bca8f
Aaron Jiane <devmfundo@gmail.com>
2025-07-23 23:47:09 +0200

    2.19*: Data for countries, step 2

--- part2/dataforcountries/src/components/DisplayResults.jsx
+++ part2/dataforcountries/src/components/DisplayResults.jsx
@@ -5,7 +5,24 @@ const DisplayResults=({country,countries})=>{
 
   const filteredData=countries.filter((item)=>item.name.common.toLowerCase().includes(country.toLowerCase()))
 
-  const handleVisibility=()=>{}
+    const handleShow=(countryName)=>{
+    setVisibility(countryName)
+  }
+  
+
+
+  const renderCountry=(country)=>{
+    return(
+      <div>
+        <h2>{country.name.common}</h2>
+        <p>Capital {country.capital}</p>
+        <p>Area {country.area}</p>
+        <h2>Languages</h2>
+        <ul>{Object.values(country.languages).map((language)=><li key={language}>{language}</li>)}</ul>
+        <img src={country.flags.png} alt={`${country.name.common}`}/>
+      </div>
+    )
+  }
 
   const searchResults=()=>{
   if(!country){
@@ -20,32 +37,27 @@ const DisplayResults=({country,countries})=>{
     return (
       <>
         {filteredData.map(item => (
-          <p key={item.name.common}>{item.name.common} <button>Show</button></p>
+          <div key={item.name.common}>
+            {item.name.common}
+            <button onClick={()=>handleShow(item.name.common)}>Show</button>
+            {visibility === item.name.common && renderCountry(item)}
+          </div>
+          
         ))}
       </>
     )
   }
 
   if(filteredData.length === 1){
-    const country = filteredData[0]
-    return(
-      <div>
-        <h2>{country.name.common}</h2>
-        <p>Capital {country.capital}</p>
-        <p>Area {country.area}</p>
-        <h2>Languages</h2>
-        <ul>{Object.values(country.languages).map((language)=><li key={language}>{language}</li>)}</ul>
-        <img src={country.flags.png} alt={`${country.name.common}`}/>
-      </div>
-    )
+    return renderCountry(filteredData[0])
   }
 
   return null
 }
-const display = searchResults()
+ 
 	return(
 	<>
-	{display}
+	{searchResults()}
 	</>
 	)
 }
