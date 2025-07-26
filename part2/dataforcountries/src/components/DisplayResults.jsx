3979b1128539fe85ba4e32e70fe88b7f7e7cdd96
Aaron Jiane <devmfundo@gmail.com>
2025-07-26 18:00:23 +0200

    2.20*: Data for countries, step 3

--- part2/dataforcountries/src/components/DisplayResults.jsx
+++ part2/dataforcountries/src/components/DisplayResults.jsx
@@ -1,14 +1,32 @@
-import {useState} from 'react'
+import {useState,useEffect} from 'react'
+import services from './services/api_services'
+import Notification from './Notification'
 
 const DisplayResults=({country,countries})=>{
   const [visibility, setVisibility] = useState(null)
+  const [weather, setWeather] = useState(null)
+  const [latLong, setLatLong] = useState(null)
+  const [message, setMessage] = useState("")
 
+  
+  useEffect(()=>{
+    if(!latLong)return
+      services.getWeather(latLong)
+    .then((res)=>{
+      setWeather(res.data)
+    })
+    .catch((err)=>{
+      setMessage("Failed to fetch weather")
+    })
+  },[latLong])
+  
   const filteredData=countries.filter((item)=>item.name.common.toLowerCase().includes(country.toLowerCase()))
 
-    const handleShow=(countryName)=>{
-    setVisibility(countryName)
+    const handleShow=(country)=>{
+    setVisibility(country.name.common)
+    setLatLong(country.latlng)
+    setWeather(null)
   }
-  
 
 
   const renderCountry=(country)=>{
@@ -20,6 +38,20 @@ const DisplayResults=({country,countries})=>{
         <h2>Languages</h2>
         <ul>{Object.values(country.languages).map((language)=><li key={language}>{language}</li>)}</ul>
         <img src={country.flags.png} alt={`${country.name.common}`}/>
+        <h2>Weather in {country.name.common}</h2>
+        {weather ? (
+        <>
+        <p>Temperature {weather.main.temp} Celcius</p>
+        <img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+        alt="weather forecast icon"/>
+        <p>Wind {weather.wind.speed} m/s</p>
+        </>
+        ):(<p>Loading weather</p>)} 
+        {!weather && (
+          <>
+          <Notification message={message}/>
+          </>
+          )}
       </div>
     )
   }
@@ -38,8 +70,8 @@ const DisplayResults=({country,countries})=>{
       <>
         {filteredData.map(item => (
           <div key={item.name.common}>
-            {item.name.common}
-            <button onClick={()=>handleShow(item.name.common)}>Show</button>
+            {item.name.common}{" "}
+            <button onClick={()=>handleShow(item)}>Show</button>
             {visibility === item.name.common && renderCountry(item)}
           </div>
           
@@ -48,9 +80,13 @@ const DisplayResults=({country,countries})=>{
     )
   }
 
-  if(filteredData.length === 1){
-    return renderCountry(filteredData[0])
-  }
+   if (filteredData.length === 1) {
+      const singleCountry = filteredData[0]
+      if (latLong !== singleCountry.latlng) {
+        setLatLong(singleCountry.latlng)
+      }
+      return renderCountry(singleCountry)
+    }
 
   return null
 }
--- part2/dataforcountries/src/components/services/api_services.jsx
+++ part2/dataforcountries/src/components/services/api_services.jsx
@@ -1,9 +1,20 @@
 import axios from 'axios'
 
-const URL_LINK = import.meta.env.VITE_COUNTRY_API
+const COUNTRY_API_URL = import.meta.env.VITE_COUNTRY_API
+const OPEN_WEATHER_API_KEY = import.meta.env.VITE_OPEN_WEATHER_API_KEY
 
-const getCountries=()=>axios.get(`${URL_LINK}`)
+const getCountries = () => axios.get(COUNTRY_API_URL);
 
-const services = {getCountries}
+const getWeather = ([lat, lon]) =>
+  axios.get(`https://api.openweathermap.org/data/2.5/weather`, {
+    params: {
+      lat,
+      lon,
+      units: 'metric',
+      appid: OPEN_WEATHER_API_KEY,
+    },
+  })
+
+const services = { getCountries, getWeather }
 
 export default services
