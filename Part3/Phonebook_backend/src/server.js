c201843cfb77ec52b36c8c24fe2e329aa7650d61
Aaron Jiane <devmfundo@gmail.com>
2025-07-30 17:06:29 +0200

    Phonebook backend step 3

--- Part3/Phonebook_backend/src/server.js
+++ Part3/Phonebook_backend/src/server.js
@@ -19,7 +19,6 @@ app.get('/api/persons/:id',(req,res)=>{
 	const person = persons.find(person=>person.id===id)
 	if(!person)res.status(404).json({error: "Person not found"})
 	res.status(200).json(person)
-
 })
 
 app.get('/info',(req,res)=>{
