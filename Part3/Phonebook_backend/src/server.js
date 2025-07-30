54f4f273c5e52008e08b3ddf5a6d177ba3e348cb
Aaron Jiane <devmfundo@gmail.com>
2025-07-30 19:52:41 +0200

    3.4: Phonebook backend step 4

--- Part3/Phonebook_backend/src/server.js
+++ Part3/Phonebook_backend/src/server.js
@@ -17,8 +17,17 @@ app.get('/api/persons', (req,res)=>{
 app.get('/api/persons/:id',(req,res)=>{
 	const id = req.params.id
 	const person = persons.find(person=>person.id===id)
-	if(!person)res.status(404).json({error: "Person not found"})
+	if(!person)return res.status(404).json({error: "Person not found"})
 	res.status(200).json(person)
+})
+
+app.delete('/api/persons/:id',(req,res)=>{
+	const id = req.params.id
+	const index = persons.findIndex(person=>person.id===id)
+	if(index === -1)return res.status(401).json({error: "Failed to delete"})
+	persons.splice(index,1)
+	res.status(204).end()
+
 })
 
 app.get('/info',(req,res)=>{
