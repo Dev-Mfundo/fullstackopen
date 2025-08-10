3fc1ee835ba97ed3e48c2e06d5090f6acf0730cd
Aaron Jiane <devmfundo@gmail.com>
2025-08-10 02:18:51 +0200

    deploy fix

--- part2/phonebook/vite.config.js
+++ part2/phonebook/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
+// vite.config.js
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: 10000,
+    host: true,
+    allowedHosts: ['phonebook-frontend-gvjh.onrender.com']
+  }
 })
